4585d73cd0d328bc8873e362b3774bd06b8deb73
BhuvaneswaranMurugesan <bhuvaneswaran.kanini@gmail.com>
2026-05-06 00:35:19 +0530

    [OOPS] - Day2 task 3 completed

---
+++ OOPS/Day_02/03_task.py
@@ -0,0 +1,17 @@
+# Bug Simulation
+
+class Team:
+    memebers = []
+    
+    def __init__(self):
+        pass
+
+
+t1 = Team()
+t1.memebers.append("John")
+
+t2 = Team()
+t2.memebers.append("Doe")
+
+print(t1.memebers)
+print(t2.memebers)
