8416ee17da4added7a2b066dd1cb0e5391b015c4
BhuvaneswaranMurugesan <bhuvaneswaran.kanini@gmail.com>
2026-05-06 00:15:21 +0530

    [OOPS] - Day2 task 2 completed

---
+++ OOPS/Day_02/02_task.py
@@ -0,0 +1,15 @@
+class Company:
+    
+    company_name = "ABC Corp"
+    
+    def __init__(self,name):
+        self.employee_name = name
+
+
+c1 = Company("John")
+c2 = Company("Doe")
+
+c1.company_name = "Corp"
+
+print(c1.company_name)
+print(c2.company_name)
