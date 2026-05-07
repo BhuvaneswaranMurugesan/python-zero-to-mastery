587d4ba9664e7488a7df2225ba524143125655e3
BhuvaneswaranMurugesan <bhuvaneswaran.kanini@gmail.com>
2026-05-08 00:42:57 +0530

    [Day2] - static method

---
+++ OOPS/Day_02/static_method.py
@@ -0,0 +1,13 @@
+
+class Math_function:
+    
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+m = Math_function()
+print(m.add(5, 3))
